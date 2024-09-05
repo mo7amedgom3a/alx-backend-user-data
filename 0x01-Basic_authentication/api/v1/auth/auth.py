444bcd701620de8e844fa108bd87eafd9cf4cd2f
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-09-05 18:43:52 +0300

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,18 +2,40 @@
 """
 Module for authentication
 """
-from flask import request
+
+
 from typing import List, TypeVar
+from flask import request
 
 
 class Auth:
-    """Auth class
+    """_summary_
     """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """_summary_
+
+        Args:
+                path (str): _description_
+                excluded_paths (List[str]): _description_
+
+        Returns:
+                bool: _description_
+        """
         return False
 
-    def authorization_header(self, request=None) -> str:
+    def authorization_header(self) -> str:
+        """_summary_
+
+        Returns:
+                str: _description_
+        """
         return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
+    def current_user(self) -> TypeVar('User'):
+        """_summary_
+
+        Returns:
+                TypeVar('User'): _description_
+        """
         return None
