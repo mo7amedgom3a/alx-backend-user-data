83fff707b52fafde8bbf20eabf7d0f8914e938f4
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-09-05 18:35:08 +0300

    forbidden endpoint

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,6 +24,16 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """_summary_
+
+    Returns:
+        str: _description_
+    """
+    abort(403, description='Forbidden')
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
